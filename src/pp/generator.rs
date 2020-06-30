6891bac2bc7a167154a8623553016d6fd35e07f9
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2020-06-30 14:58:36 +0200

    Work on the pre-preprocessing interpreter

--- src/pp/generator.rs
+++ src/pp/generator.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 use crate::crypto::RingHasher;
+use crate::Instruction;
 
 use std::io::Write;
 
@@ -38,8 +39,7 @@ pub fn correction_hash<'a, D: Domain, R: RngCore>(rngs: &'a mut [R], batches: u6
 }
 
 /// Implementation of pre-processing phase used by the prover during online execution
-pub struct ProverNew<'a, D: Domain, W: Write, R: RngCore, const N: usize> {
-    read: usize,
+pub struct BeaverStack<'a, D: Domain, W: Write, R: RngCore, const N: usize> {
     share_a: Vec<D::Sharing>,
     share_b: Vec<D::Sharing>,
     share_c: Vec<D::Sharing>,
@@ -47,19 +47,18 @@ pub struct ProverNew<'a, D: Domain, W: Write, R: RngCore, const N: usize> {
     rngs: Box<[R; N]>, // rngs for players
 }
 
-impl<'a, D: Domain, W: Write, R: RngCore, const N: usize> ProverNew<'a, D, W, R, N> {
+impl<'a, D: Domain, W: Write, R: RngCore, const N: usize> BeaverStack<'a, D, W, R, N> {
     pub fn new(rngs: Box<[R; N]>, zero: &'a mut W) -> Self {
         Self {
-            read: 0,
             share_a: Vec::with_capacity(D::Batch::DIMENSION),
             share_b: Vec::with_capacity(D::Batch::DIMENSION),
-            share_c: Vec::with_capacity(D::Batch::DIMENSION),
+            share_c: vec![D::Sharing::ZERO; D::Batch::DIMENSION],
             zero,
             rngs,
         }
     }
 
-    fn generate(&mut self) {
+    pub fn generate(&mut self) {
         debug_assert_eq!(self.share_a.len(), D::Batch::DIMENSION);
         debug_assert_eq!(self.share_b.len(), D::Batch::DIMENSION);
         debug_assert_eq!(self.share_c.len(), 0);
@@ -92,7 +91,6 @@ impl<'a, D: Domain, W: Write, R: RngCore, const N: usize> ProverNew<'a, D, W, R,
         batches_c[0].serialize(&mut self.zero).unwrap();
 
         // transpose c back into D::Batch::DIMENSION sharings
-        self.share_c.resize(D::Batch::DIMENSION, D::Sharing::ZERO);
         D::convert(&mut self.share_c[..], &batches_c[..]);
 
         // remove input shares from internal buffer
@@ -100,24 +98,105 @@ impl<'a, D: Domain, W: Write, R: RngCore, const N: usize> ProverNew<'a, D, W, R,
         self.share_b.clear();
     }
 
-    pub fn append(&mut self, a: D::Sharing, b: D::Sharing) -> bool {
+    pub fn push(&mut self, a: D::Sharing, b: D::Sharing) {
+        assert!(self.share_a.len() < D::Batch::DIMENSION);
+        debug_assert_eq!(self.share_a.len(), self.share_b.len());
         self.share_a.push(a);
         self.share_b.push(b);
         if self.share_a.len() >= D::Batch::DIMENSION {
             self.generate();
-            true
-        } else {
-            false
         }
     }
 
-    pub fn read(&mut self) -> Option<D::Sharing> {
-        if self.read >= self.share_c.len() {
-            self.read = 0;
+    pub fn pop(&mut self) -> Option<D::Sharing> {
+        assert_eq!(self.share_a.len(), D::Batch::DIMENSION);
+        if self.share_c.len() == 1 {
+            self.share_a.clear();
+            self.share_b.clear();
+        }
+        self.share_c.pop()
+    }
+}
+
+/// Implementation of pre-processing phase used by the prover during online execution
+pub struct PreprocessingExecution<'a, D: Domain, W: Write, R: RngCore, const N: usize> {
+    stack: BeaverStack<'a, D, W, R, N>,
+    wires: Vec<Option<D::Sharing>>,
+    mults: Vec<usize>,
+}
+
+impl<'a, D: Domain, W: Write, R: RngCore, const N: usize> PreprocessingExecution<'a, D, W, R, N> {
+    pub fn new(rngs: Box<[R; N]>, zero: &'a mut W, inputs: usize) -> Self {
+        PreprocessingExecution {
+            stack: BeaverStack::new(rngs, zero),
+            wires: Vec::new(),
+            mults: Vec::with_capacity(D::Batch::DIMENSION),
+        }
+    }
+
+    fn empty_stack(&mut self) {
+        let mut dummy: usize = 0;
+
+        // add padding sharings to each batch dimension
+        while self.mults.len() < D::Batch::DIMENSION {
+            self.stack.push(D::Sharing::ZERO, D::Sharing::ZERO);
+            dummy += 1;
         }
 
-        let elem = self.share_c[self.read];
-        Some(elem)
+        // generate output sharings
+        self.stack.generate();
+
+        // strip dummy sharings
+        for _ in 0..dummy {
+            self.stack.pop();
+        }
+
+        // write back the new sharings
+        while let Some(idx) = self.mults.pop() {
+            self.wires[idx] = self.stack.pop();
+        }
+    }
+
+    fn compute(&mut self, idx: usize) -> D::Sharing {
+        if let Some(mask) = self.wires[idx] {
+            mask
+        } else {
+            // otherwise it is on the beaver stack
+            self.empty_stack();
+            self.wires[idx].unwrap()
+        }
+    }
+
+    pub fn step(&mut self, ins: &Instruction<<D::Sharing as RingModule>::Scalar>) {
+        match ins {
+            Instruction::AddConst(_dst, _src, _c) => (), // noop in pre-processing
+            Instruction::MulConst(dst, src, c) => {
+                let sw = self.compute(*src);
+                self.wires[*dst] = Some(sw.action(*c))
+            }
+            Instruction::Add(dst, src1, src2) => {
+                let sw1 = self.compute(*src1);
+                let sw2 = self.compute(*src2);
+                self.wires[*dst] = Some(sw1 + sw2);
+            }
+            Instruction::Mul(dst, src1, src2) => {
+                let sw1 = self.compute(*src1);
+                let sw2 = self.compute(*src2);
+
+                // check if space on Beaver stack
+                if self.mults.len() >= D::Batch::DIMENSION {
+                    self.empty_stack();
+                }
+
+                // push the masks to the Beaver stack
+                self.stack.push(sw1, sw2);
+                self.mults.push(*dst);
+
+                // mark destination as pending computation
+                self.wires[*dst] = None;
+            }
+            Instruction::Output(_src) => (),
+        }
     }
 }
 
