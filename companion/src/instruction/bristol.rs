cf22e86e488f90f3a8ae0dfc6067dad50fe094ff
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2021-01-15 12:45:33 -0800

    Implement fromstr for the bristol header

--- companion/src/instruction/bristol.rs
+++ companion/src/instruction/bristol.rs
@@ -6,79 +6,78 @@ use reverie::algebra::RingElement;
 use reverie::Instruction;
 
 use super::Parser;
+use std::str::FromStr;
 
-pub struct InsParser {
-    line: String,
-    reader: BufReader<File>,
+pub struct BristolHeader {
     pub n_gate: usize,
     pub n_wire: usize,
     pub n_input: usize,
     pub n_output: usize,
-    pending_input: usize,
-    pending_output: usize,
+    pub pending_input: usize,
+    pub pending_output: usize,
 }
 
-fn parse_header(l1: String, l2: String, l3: String) -> (usize, usize, usize, usize) {
-    let mut parts = l1.split(' ');
-    let n_gate: usize = parts.next().unwrap().trim().parse().unwrap();
-    let n_wire: usize = parts.next().unwrap().trim().parse().unwrap();
-
-    parts = l2.split(' ');
-    let mut n_input = 0;
-    let n_input_wires = parts.next().unwrap().trim().parse().unwrap();
-    for i in 0..n_input_wires {
-        let vec_size: usize = parts
-            .next()
-            .unwrap_or_else(|| {
-                panic!(
-                    "Expected {} input vectors, only found {}",
-                    n_input_wires,
-                    i + 1
-                )
-            })
-            .trim()
-            .parse()
-            .unwrap();
-        n_input += vec_size;
+impl BristolHeader {
+    fn add_input(&mut self) {
+        self.pending_input -= 1;
+        self.n_input += 1;
     }
 
-    parts = l3.split(' ');
-    let mut n_output = 0;
-    let n_output_wires = parts.next().unwrap().trim().parse().unwrap();
-    for i in 0..n_output_wires {
-        let vec_size: usize = parts
-            .next()
-            .unwrap_or_else(|| {
-                panic!(
-                    "Expected {} output vectors, only found {}",
-                    n_output_wires,
-                    i + 1
-                )
-            })
-            .trim()
-            .parse()
-            .unwrap();
-        n_output += vec_size;
+    fn add_output(&mut self) {
+        self.pending_output -= 1;
+        self.n_output += 1;
     }
-
-    (n_gate, n_wire, n_input, n_output)
 }
 
-impl Parser<Instruction<BitScalar>> for InsParser {
-    fn new(mut reader: BufReader<File>) -> io::Result<Self> {
-        let mut l1 = String::with_capacity(128);
-        reader.read_line(&mut l1)?;
+impl FromStr for BristolHeader {
+    type Err = std::io::Error;
 
-        let mut l2 = String::with_capacity(512);
-        reader.read_line(&mut l2)?;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.trim().split('\n');
+        let mut parts = lines.next().unwrap().split(' ');
 
-        let mut l3 = String::with_capacity(512);
-        reader.read_line(&mut l3)?;
+        let n_gate: usize = parts.next().unwrap().trim().parse().unwrap();
+        let n_wire: usize = parts.next().unwrap().trim().parse().unwrap();
 
-        let (n_gate, n_wire, n_input, n_output) = parse_header(l1, l2, l3);
-        Ok(InsParser {
-            line: String::with_capacity(128),
-            reader,
+        parts = lines.next().unwrap().split(' ');
+        let mut n_input = 0;
+        let n_input_wires = parts.next().unwrap().trim().parse().unwrap();
+        for i in 0..n_input_wires {
+            let vec_size: usize = parts
+                .next()
+                .unwrap_or_else(|| {
+                    panic!(
+                        "Expected {} input vectors, only found {}",
+                        n_input_wires,
+                        i + 1
+                    )
+                })
+                .trim()
+                .parse()
+                .unwrap();
+            n_input += vec_size;
+        }
+
+        parts = lines.next().unwrap().split(' ');
+        let mut n_output = 0;
+        let n_output_wires = parts.next().unwrap().trim().parse().unwrap();
+        for i in 0..n_output_wires {
+            let vec_size: usize = parts
+                .next()
+                .unwrap_or_else(|| {
+                    panic!(
+                        "Expected {} output vectors, only found {}",
+                        n_output_wires,
+                        i + 1
+                    )
+                })
+                .trim()
+                .parse()
+                .unwrap();
+            n_output += vec_size;
+        }
+
+        Ok(BristolHeader {
             n_gate,
             n_wire,
             n_input: 0,
@@ -87,27 +86,55 @@ impl Parser<Instruction<BitScalar>> for InsParser {
             pending_output: n_output,
         })
     }
+}
+
+pub struct InsParser {
+    line: String,
+    reader: BufReader<File>,
+    pub header: BristolHeader,
+}
+
+impl Parser<Instruction<BitScalar>> for InsParser {
+    fn new(mut reader: BufReader<File>) -> io::Result<Self> {
+        let mut header_raw = String::with_capacity(2048);
+        reader.read_line(&mut header_raw)?;
+        reader.read_line(&mut header_raw)?;
+        reader.read_line(&mut header_raw)?;
+
+        let header: BristolHeader = header_raw.parse()?;
+
+        Ok(InsParser {
+            line: String::with_capacity(128),
+            reader,
+            header,
+        })
+    }
 
     fn next(&mut self) -> io::Result<Option<Instruction<BitScalar>>> {
-        if self.pending_input > 0 {
-            let idx = self.n_input;
-            self.n_input += 1;
-            self.pending_input -= 1;
+        // Add input gates at the beginning of the circuit
+        if self.header.pending_input > 0 {
+            let idx = self.header.n_input;
+            self.header.add_input();
             return Ok(Some(Instruction::Input(idx)));
         }
 
         self.line.clear();
         self.reader.read_line(&mut self.line)?;
+
+        // Add output gates at the end of the circuit
         if self.line.is_empty() {
-            if self.pending_output > 0 {
-                let idx = self.n_output;
-                let total_outputs = self.n_output + self.pending_output;
-                self.n_output += 1;
-                self.pending_output -= 1;
-                return Ok(Some(Instruction::Output(self.n_wire - total_outputs + idx)));
+            if self.header.pending_output > 0 {
+                let idx = self.header.n_output;
+                let total_outputs = self.header.n_output + self.header.pending_output;
+                self.header.add_output();
+                return Ok(Some(Instruction::Output(
+                    self.header.n_wire - total_outputs + idx,
+                )));
             }
             return Ok(None);
         }
+
+        // Add all the other gates in the middle
         let mut parts = self.line.split(' ');
         parts.next().unwrap();
         parts.next().unwrap();
